61223c8b375b352f8ce96d6faf1c33a0edea57b0
oly2o6 <calumroberts02@hotmail.com>
2026-06-12 01:51:38 +0100

    WIP enderio grinding balls

---
+++ kubejs/server_scripts/mods/EnderIO/custom.js
@@ -0,0 +1,15 @@
+EnderIOEvents.grindingBalls(allthemods => {
+
+ // registers an iron ingot as a Grinding Ball
+ // it has a 1.5x output multiplier, the default 1.0x bonus multiplier,
+ // a 0.75x energy multiplier and a durability of 500
+ //   event.add("minecraft:iron_ingot", 1.5, 1.0, 0.75, 500)
+
+allthemods.add('allthemodium:allthemodium_ingot', 1.85, 1.75, 0.7, 2500)
+allthemods.add('allthemodium:vibranium_ingot', 2, 1.9, 0.6, 5000)
+allthemods.add('allthemodium:unobtainium_ingot', 2.25, 2.1, 0.5, 7500)
+
+})
+
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods projects.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
